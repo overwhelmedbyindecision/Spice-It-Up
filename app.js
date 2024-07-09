a4031e531e577b6c16d10e2eb0272f21c9c5d733
Cong Wang <wangcong525@hotmail.com>
2024-07-08 22:25:04 -0400

    feature resipes updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const year = 2024;
 const today = dayjs();
 
 const getHolidaysForNextWeek=function(){
-        const endOfWeek = dayjs(today)+7; 
+
         const requestUrl = `https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${country}&year=${year}`;
         fetch(requestUrl)
         .then(function (response) {
@@ -16,7 +16,7 @@ const getHolidaysForNextWeek=function(){
                 const holidayList = document.createElement("div");
                 let count=0;
                 for (const holiday of holidays){
-                    if (dayjs(holiday.date.iso)>today && dayjs(holiday.date.iso)<today.add(7,"day")) {
+                    if (dayjs(holiday.date.iso)>today && dayjs(holiday.date.iso)<today.add(10,"day")) {
                 const listItem=document.createElement("li");
                 const listDesc=document.createElement("p");
                 listItem.textContent=`${holiday.name}: ${holiday.date.iso}`;
--- assets/holiday.js
+++ assets/holiday.js
@@ -1,35 +1,49 @@
-const apiKey = '8BNwUuq4RtWSDwnixUF4E3nEIkV1oPQx';
-const country = 'US';
+const apiKey = 'lGbpFvBDHMya0SxraF514q9D9Za18ZWq';
+const country = document.getElementById("country");
+
+
 const year = 2024;
 const today = dayjs();
 
 const getHolidaysForNextWeek=function(){
-        const endOfWeek = dayjs(today)+7; 
-        const requestUrl = `https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${country}&year=${year}`;
-        fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-          })
-        .then(
-            function (data) {
-                const holidays = data.response.holidays;
-                const holidayList = document.createElement("div");
-                let count=0;
-                for (const holiday of holidays){
-                    if (dayjs(holiday.date.iso)>today && dayjs(holiday.date.iso)<today.add(7,"day")) {
-                const listItem=document.createElement("li");
-                const listDesc=document.createElement("p");
-                listItem.textContent=`${holiday.name}: ${holiday.date.iso}`;
-                listDesc.textContent=`${holiday.description}`;
-                listItem.appendChild(listDesc);
-                holidayList.appendChild(listItem);
-                console.log(holidayList);
-                }
+    const endOfWeek = dayjs(today)+7; 
+    const requestUrl = `https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${country.value}&year=${year}`;
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(
+       
+        function (data) {
+            console.log(data);
+            const holidays = data.response.holidays;
+            const holidayList = document.createElement("div");
+            holidayList.classList.add("holiday-list"); 
+            for (const holiday of holidays){
+                if (dayjs(holiday.date.iso)>today && dayjs(holiday.date.iso)<today.add(10,"day")) {
+            const holidayCard = document.createElement("div");
+            holidayCard.classList.add("holiday-card"); 
+
+            const cardHeader = document.createElement("h3");
+            const cardDate = dayjs(holiday.date.iso).format("MMM-DD"); 
+            cardHeader.textContent = cardDate;
+            holidayCard.appendChild(cardHeader);
+
+            const cardDesc = document.createElement("h4");
+            cardDesc.textContent = holiday.name;
+            holidayCard.appendChild(cardDesc);
+
+            const cardDescFull = document.createElement("p");
+            cardDescFull.textContent = `Description: ${holiday.description}`;
+            holidayCard.appendChild(cardDescFull);
+
+            holidayList.appendChild(holidayCard);
             }
-            document.body.appendChild(holidayList);
         }
-    );
-        
-    }         
+        document.body.appendChild(holidayList);
+    }
+);
+    
+}        
 
 getHolidaysForNextWeek();
